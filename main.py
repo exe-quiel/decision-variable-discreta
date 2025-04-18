97298d3ecb8114f64df086d3c2df5a593b7900cc
Exequiel <exequielamelotti@hotmail.com>
2025-04-18 02:24:01 -0300

    Experimento con la interfaz

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.simpledialog
 from tkinter.ttk import Notebook, Combobox, Style
 
 window = tk.Tk()
@@ -14,7 +15,7 @@ window.title('Analizador')
 
 nb = Notebook(window)
 
-frame_analisis = tk.Frame(nb)
+frame_datos = tk.Frame(nb)
 
 '''
 label_frame = tk.LabelFrame(frame_analisis, text="TEXT")
@@ -42,19 +43,41 @@ style.theme_settings("default", {
 #combo = Combobox(window, values=['Wald (maximax)', 'Optimista (maximin)', 'Hurwicz', 'Savage (minimax)', 'Máximo beneficio esperado'])
 #combo.pack()
 
-n = 5
+def leer_string_y_reemplazar(event):
+    nuevo_texto = tk.simpledialog.askstring('Ingrese nuevo texto', 'Nuevo texto')
+    event.widget['text'] = nuevo_texto
+
+def agregar_columna(event):
+    print(event.widget)
+    pass
+
+def agregar_fila(event):
+    print(event.widget)
+    pass
+
+n = 4
 
 for row in range(n):
-    label_letra = tk.Label(frame_analisis, text=chr(ord('A') + row))
-    label_letra.grid(column=2 + row, row=0)
-    label_number = tk.Label(frame_analisis, text=chr(ord('1') + row))
-    label_number.grid(column=1, row=1 + row)
+    label_letra = tk.Label(frame_datos, text=chr(ord('A') + row), width=5)
+    label_letra.grid(column=2 + row, row=0, padx=0, pady=0)
+    label_letra.bind('<Button-1>', leer_string_y_reemplazar)
+    label_number = tk.Label(frame_datos, text=chr(ord('1') + row), width=5)
+    label_number.grid(column=1, row=1 + row, padx=0, pady=0)
+    label_number.bind('<Button-1>', leer_string_y_reemplazar)
     for column in range(0, n):
-        t = tk.Entry(frame_analisis, width=5)
+        t = tk.Entry(frame_datos, width=10)
         #t = tk.Label(f, width=5, background='#ffffff', border=1, borderwidth=1)
-        t.grid(column=column + 2, row=row + 1)
+        t.grid(column=column + 2, row=row + 1, pady=0)
 
-nb.add(frame_analisis, text='Ingreso de datos')
+btn_agregar_columna = tk.Button(frame_datos, text='+', width=2)
+btn_agregar_columna.grid(column=n + 2, row=0, padx=10)
+btn_agregar_columna.bind('<Button-1>', agregar_columna)
+btn_agregar_fila = tk.Button(frame_datos, text='+', width=2)
+btn_agregar_fila.grid(column=1, row=n + 1)
+btn_agregar_fila.bind('<Button-1>', agregar_fila)
+
+
+nb.add(frame_datos, text='Ingreso de datos')
 nb.add(tk.Label(nb, text='label 2'), text='Análisis')
 
 nb.pack(fill='both')
